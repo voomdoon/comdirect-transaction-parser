7c442250afb6b913c175996fb94428c1dd08d305
voomdoon <nilreb24-github20141105@yahoo.de>
2024-04-21 19:58:15 +0200

    perf: reduce amount and area of text reads

--- src/main/java/de/voomdoon/finance/budgetbook/parser/transaction/comdirect/ComdirectTransactionParser.java
+++ src/main/java/de/voomdoon/finance/budgetbook/parser/transaction/comdirect/ComdirectTransactionParser.java
@@ -63,11 +63,6 @@ public class ComdirectTransactionParser {
 	/**
 	 * @since 0.1.0
 	 */
-	private static final int WIDTH = 595;
-
-	/**
-	 * @since 0.1.0
-	 */
 	private static final int Y_MIN = 50;
 
 	/**
@@ -98,7 +93,12 @@ public class ComdirectTransactionParser {
 	/**
 	 * @since 0.1.0
 	 */
-	private int yDebugOffset = 10;
+	private int xLeft = 58;
+
+	/**
+	 * @since 0.1.0
+	 */
+	private int yDebugOffsetForText = 10;
 
 	/**
 	 * DOCME add JavaDoc for constructor ComdirectTransactionParser
@@ -155,8 +155,9 @@ public class ComdirectTransactionParser {
 	private LastEnd addTransactionsFromAccount(List<BankStatementTransaction> result, Account account, LastEnd lastEnd)
 			throws IOException {
 		logger.debug("addTransactionsFromAccount " + account + " " + lastEnd);
-		int yStart = findHeightFromTop(reader, lastEnd.pageIndex(), account.name(), lastEnd.y());
-		yStart = findHeightFromTop(reader, lastEnd.pageIndex(), "Alter Saldo", yStart);
+		int yStart = findHeightFromTop(reader, lastEnd.pageIndex(), account.name(), 50, 300, lastEnd.y());
+		int yOffsetOptimization = 40;
+		yStart = findHeightFromTop(reader, lastEnd.pageIndex(), "Alter Saldo", 55, 110, yStart - yOffsetOptimization);
 		logger.debug("yStart: " + yStart);
 
 		LastEnd current = new LastEnd(lastEnd.pageIndex(), yStart);
@@ -166,7 +167,7 @@ public class ComdirectTransactionParser {
 		while (true) {
 			LastEnd temp = current;
 			int yNewBalance = newBalanceYCache.computeIfAbsent(current.pageIndex,
-					key -> findHeightFromTop(reader, temp.pageIndex(), "Neuer Saldo", temp.y()));
+					key -> findHeightFromTop(reader, temp.pageIndex(), "Neuer Saldo", xLeft, 110, temp.y()));
 
 			current = maybeAddTransaction(result, account, current, yNewBalance);
 			logger.debug("current: " + current);
@@ -312,20 +313,6 @@ public class ComdirectTransactionParser {
 	}
 
 	/**
-	 * DOCME add JavaDoc for method searchHeight
-	 * 
-	 * @param reader
-	 * @param pageIndex
-	 * @param string
-	 * @return
-	 * @throws IOException
-	 * @since 0.1.0
-	 */
-	private int findHeightFromTop(PdfReader reader, int pageIndex, String string) {
-		return findHeightFromTop(reader, pageIndex, string, HEIGHT);
-	}
-
-	/**
 	 * DOCME add JavaDoc for method findHeight
 	 * 
 	 * @param reader
@@ -333,39 +320,24 @@ public class ComdirectTransactionParser {
 	 * @param string
 	 * @param yFrom
 	 * @return
-	 * @throws IOException
 	 * @since 0.1.0
 	 */
-	private int findHeightFromTop(PdfReader reader, int pageIndex, String string, int yFrom) {
-		return findHeightFromTop(reader, pageIndex, string, 0, WIDTH, yFrom);
-	}
-
-	/**
-	 * DOCME add JavaDoc for method findHeight
-	 * 
-	 * @param reader
-	 * @param pageIndex
-	 * @param content
-	 * @param yFrom
-	 * @return
-	 * @since 0.1.0
-	 */
-	private int findHeightFromTop(PdfReader reader, int pageIndex, String content, int x0, int x1, int yFrom) {
+	private int findHeightFromTop(PdfReader reader, int pageIndex, String string, int x0, int x1, int yFrom) {
 		// TODO use optional
 		StringBuilder sb = new StringBuilder();
-		sb.append("findHeightFromTop y=").append(yFrom).append(" '").append(content).append("' @").append(pageIndex)
+		sb.append("findHeightFromTop y=").append(yFrom).append(" '").append(string).append("' @").append(pageIndex)
 				.append(":");
 
 		// OPTIMIZE speed: maybe extract all rows at once
 		for (int y = yFrom; y >= Y_MIN; y--) {
 			Rectangle rectangle = new Rectangle(x0, y, x1 - x0, 1);
-			String result = reader.readText(pageIndex, rectangle);
+			String result = readText(pageIndex, rectangle, false);
 
 			if (!result.isEmpty()) {
 				sb.append("\n").append(y).append(": '").append(result).append("'");
 			}
 
-			if (result.contains(content)) {
+			if (result.contains(string)) {
 				highlightMatch(pageIndex, rectangle);
 				logger.trace(sb.toString());
 				return y;
@@ -388,7 +360,7 @@ public class ComdirectTransactionParser {
 		PdfGraphics graphics = PdfGraphics.create(reader.getDocument(), reader.getDocument().getPage(pageIndex));
 		graphics.setAlpha(0.25F);
 		graphics.setStrokingColor(Color.GREEN);
-		graphics.drawRectangle(translate(rectangle, 0, yDebugOffset));
+		graphics.drawRectangle(translate(rectangle, 0, yDebugOffsetForText));
 		graphics.close();
 	}
 
@@ -413,24 +385,25 @@ public class ComdirectTransactionParser {
 
 		int xDateRight = 113;
 
-		int yBookingDate = findHeightFromTop(reader, lastEnd.pageIndex(), ".", 0, xDateRight, lastEnd.y() - 1);
+		int yBookingDate = findHeightFromTop(reader, lastEnd.pageIndex(), ".", xLeft, xDateRight, lastEnd.y() - 1);
 
 		if (yBookingDate < yBottom) {
 			logger.debug("bottom reaced");
 			return new LastEnd(lastEnd.pageIndex(), yBottom);
 		}
 
-		String bookingDate = readText(lastEnd.pageIndex(), new Rectangle(0, yBookingDate, xDateRight, 1));
+		String bookingDate = readText(lastEnd.pageIndex(), new Rectangle(xLeft, yBookingDate, xDateRight - xLeft, 1),
+				true);
 		logger.debug("booking date: '" + bookingDate + "' @y=" + yBookingDate);
 		transaction.setBookingDate(parseLocalDate(bookingDate));
 
 		int yValuta = findHeightFromTop(reader, lastEnd.pageIndex(), ".", 0, xDateRight, yBookingDate - 1);
-		String valuta = readText(lastEnd.pageIndex(), new Rectangle(0, yValuta, xDateRight, 1));
+		String valuta = readText(lastEnd.pageIndex(), new Rectangle(xLeft, yValuta, xDateRight - xLeft, 1), true);
 		logger.debug("valuta: '" + valuta + "' @y=" + yValuta);
 		transaction.setValuta(parseLocalDate(valuta));
 
 		int yNext = findHeightFromTop(reader, lastEnd.pageIndex(), ".", 0, xDateRight, yValuta - 1);
-		String next = readText(lastEnd.pageIndex(), new Rectangle(0, yNext, xDateRight, 1));
+		String next = readText(lastEnd.pageIndex(), new Rectangle(xLeft, yNext, xDateRight - xLeft, 1), true);
 		logger.debug("next: '" + next + "' @y=" + yNext);
 		boolean last = false;
 
@@ -449,7 +422,7 @@ public class ComdirectTransactionParser {
 		int xOtherAccountRight = 309;
 
 		String otherAccount = readText(lastEnd.pageIndex(),
-				new Rectangle(xOtherAccountLeft, yNext + 1, xOtherAccountRight - xOtherAccountLeft, height));
+				new Rectangle(xOtherAccountLeft, yNext + 1, xOtherAccountRight - xOtherAccountLeft, height), true);
 		otherAccount = removeLineBreaks(otherAccount);
 		logger.debug("otherAccount: '" + otherAccount + "'");
 		transaction.setOtherAccount(otherAccount);
@@ -457,7 +430,7 @@ public class ComdirectTransactionParser {
 		int xDetailsLeft = 300;
 		int xDetailsRight = 500;
 		String details = readText(lastEnd.pageIndex(),
-				new Rectangle(xDetailsLeft, yNext + 1, xDetailsRight - xDetailsLeft, height));
+				new Rectangle(xDetailsLeft, yNext + 1, xDetailsRight - xDetailsLeft, height), true);
 		logger.trace("details: '" + details + "'");
 
 		processDetails(details, transaction);
@@ -465,7 +438,7 @@ public class ComdirectTransactionParser {
 		int xAmountLeft = 500;
 		int xAmountRight = 560;
 		String amount = readText(lastEnd.pageIndex(),
-				new Rectangle(xAmountLeft, yNext + 1, xAmountRight - xAmountLeft, height));
+				new Rectangle(xAmountLeft, yNext + 1, xAmountRight - xAmountLeft, height), true);
 		amount = removeLineBreaks(amount);
 		logger.debug("amount: '" + amount + "'");
 		transaction.setAmount(parseAmount(amount));
@@ -474,7 +447,7 @@ public class ComdirectTransactionParser {
 
 		if (last) {
 			logger.info("page end reached");
-			return new LastEnd(lastEnd.pageIndex() + 1, HEIGHT);
+			return new LastEnd(lastEnd.pageIndex() + 1, 720);
 
 		}
 
@@ -560,31 +533,44 @@ public class ComdirectTransactionParser {
 	 * @since 0.1.0
 	 */
 	private AccountsResult readAccounts(PdfReader reader) throws IOException {
-		int yStartOverview = findHeightFromTop(reader, 1, "Kontoübersicht");
+		int overviewYTop = 705;
+		int yStartOverview = findHeightFromTop(reader, 1, "Kontoübersicht", xLeft, 150, overviewYTop);
 		logger.debug("Kontoübersicht: at y=" + yStartOverview);
 
-		int yStartCurrency = findHeightFromTop(reader, 1, "Kontowährung", yStartOverview);
+		if (yStartOverview == -1) {
+			throw new IllegalStateException();
+		}
+
+		int currencyYTop = 676;
+		int yStartCurrency = findHeightFromTop(reader, 1, "Kontowährung", 420, 490, currencyYTop);
 		logger.debug("Kontowährung: at y=" + yStartCurrency);
 
-		int yEnd = findHeightFromTop(reader, 1, "Gesamtsaldo", yStartCurrency);
+		if (yStartCurrency == -1) {
+			throw new IllegalStateException();
+		}
+
+		// OPTIMIZE speed: is it better to reduce width?
+		int yEnd = findHeightFromTop(reader, 1, "Gesamtsaldo", 55, 115, yStartCurrency);
 		logger.debug("Gesamtsaldo: at y=" + yEnd);
 		// TODO implement getAccounts
 
 		int xNameRight = 200;
-		int xIbanRight = 400;
+		int xIbanLeft = 260;
+		int xIbanRight = 385;
 
 		List<Account> result = new ArrayList<>();
 
 		for (int y = yStartCurrency - 1; y > yEnd; y--) {
-			String name = readText(1, new Rectangle(0, y, xNameRight, 1));
-			String iban = readText(1, new Rectangle(xNameRight, y, xIbanRight - xNameRight, 1));
+			String name = readText(1, new Rectangle(xLeft, y, xNameRight - xLeft, 1), true);
 
 			if (!name.isEmpty()) {
+				String iban = readText(1, new Rectangle(xIbanLeft, y, xIbanRight - xIbanLeft, 1), true);
 				result.add(new Account(name, iban.replace(" ", "")));
 			}
 		}
 
-		return new AccountsResult(result, yEnd);
+		int yOffsetOptimization = 40;
+		return new AccountsResult(result, yEnd - yOffsetOptimization);
 	}
 
 	/**
@@ -595,16 +581,20 @@ public class ComdirectTransactionParser {
 	 * @return
 	 * @since 0.1.0
 	 */
-	private String readText(int pageIndex, Rectangle rectangle) {
-		PdfGraphics graphics = PdfGraphics.create(reader.getDocument(), reader.getDocument().getPage(pageIndex));
-		graphics.setAlpha(0.25F);
-		graphics.setStrokingColor(Color.RED);
-		graphics.drawRectangle(translate(rectangle, 0, yDebugOffset));
-		graphics.close();
+	private String readText(int pageIndex, Rectangle rectangle, boolean debug) {
+		if (debug) {
+			PdfGraphics graphics = PdfGraphics.create(reader.getDocument(), reader.getDocument().getPage(pageIndex));
+			graphics.setAlpha(0.25F);
+			graphics.setStrokingColor(Color.RED);
+			graphics.drawRectangle(translate(rectangle, 0, yDebugOffsetForText));
+			graphics.close();
+		}
 
 		String result = reader.readText(pageIndex, rectangle);
 
-		logger.debug("readText " + rectangle + " : '" + result + "'");
+		if (debug) {
+			logger.debug("readText " + rectangle + " : '" + result + "'");
+		}
 
 		return result;
 	}
